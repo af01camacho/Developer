1413301df46d7f4f997b2de60f404ae39be0deb0
Andres Camacho <99270915+af01camacho@users.noreply.github.com>
2022-11-19 07:23:11 -0500

    1.0

---
+++ PHP CURSOS/ejercicio1.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+     EJERCICIO # 1 - PHP DEVELOTECA
+     https://www.youtube.com/watch?v=nCB1gEkRZ1g
+*/
+    // Escribir en documento PHP
+    echo "Hola Mundo</br>";
+    print_r("Hola Mundo</br>");
+    print("Hola Mundo");
+
+
+
+
+?>
---
+++ PHP CURSOS/ejercicio2.php
@@ -0,0 +1,26 @@
+<?php
+    // Condicionales 
+    if($_POST){
+         // Metodo POST para enviar datos.
+        $nombre = $_POST['txtNombre'];
+        // Mostrar Datos de variables
+        print_r($nombre);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario ejercicio 2</title>
+</head>
+<body>
+    <!- Formulario ->
+    <form action="ejercicio2.php" method="post">
+        <!- Nombre de input para almacenar datos en variable ($nombre) ->
+        <input type="text" name="txtNombre" id="">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
